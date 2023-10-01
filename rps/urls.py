bdc81d0a38a78c9a30d1ec280fb32b97ae464b35
shree <shreyashmohod@gmail.com>
2023-10-01 18:56:41 +0530

    base

--- rps/urls.py
+++ rps/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 
-from core.views import index, score
+from core.views import index, score,signup
 
 
 urlpatterns = [
-    path('', index, name="index"),
+    path('', include('core.urls')),
     path('score/',score, name='score'),
     path("admin/", admin.site.urls),
+    # path('signup/', signup, name='signup'),
+
 ]
